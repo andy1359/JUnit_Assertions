e405fce3c49b1c4bfa6b646241149ea5b78dd3ef
Ad Smarn <hi_andrew@inbox.ru>
2016-02-27 22:05:47 -0800

    Initial commit

---
+++ src/test/java/core/AppTest.java
@@ -0,0 +1,76 @@
+package core;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class AppTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	// @Test
+	// public void test() {
+	// fail("Not yet implemented");
+	// }
+
+	@Test
+	public void Test_01_AssertEquals_Positive() 
+	{assertEquals("String not the same", "Testing JUnit 4", App.s);}
+
+	@Test
+	public void Test_02_AssertEquals_Negative() 
+	{assertEquals("String not the same", "Testing TestNG 6", App.s);}
+
+	@Ignore
+	@Test
+	public void Test_02_AssertEquals_Ignore() 
+	{assertEquals("String not the same", "Testing TestNG 3", App.s);}
+
+	@Test
+	public void Test_04_AssertSame_Positive() 
+	{assertSame("Integer not the same.", 55,App.i);}
+
+	@Test
+	public void Test_05_AssertSame_Negative() {assertSame("Integer not the same.", 54,App.i);}
+
+	@Ignore
+	@Test
+	public void Test_06_AssertSame_Ignored() 
+	{assertSame("Integer not the same.", 56,App.i);}
+
+	@Test
+	public void Test_07_AssertFalse_Positive() 
+	{assertFalse("Boolean should be false", App.f);}
+
+	@Test
+	public void Test_08_AssertFalse_Negative() 
+	{assertFalse("Boolean should be false", App.t);}
+	
+	@Test
+	public void Test_09_AssertTrue_Positive() 
+	{assertTrue("Boolean should be true", App.t);}
+
+	@Test
+	public void Test_10_AssertTrue_Negative() 
+	{assertTrue("Boolean should be true", App.f);}
+
+}
